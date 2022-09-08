73b8a1a63b4d27a637f1ad488547d10d41a059a3
hrishi-mane <manehrishikesh229@gmail.com>
2022-09-09 00:56:29 +0530

    .

--- src/main/java/com/example/blogapp/controllers/BlogController.java
+++ src/main/java/com/example/blogapp/controllers/BlogController.java
@@ -6,7 +6,6 @@ import com.example.blogapp.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import static com.example.blogapp.utils.AppConstants.*;
